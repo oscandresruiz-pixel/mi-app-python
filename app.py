98ce89c18c5d8e3069cc1e8f90458713a153f96a
oscandresruiz-pixel <oscandresruiz@gmail.com>
2026-04-29 13:23:46 -0600

    Fix import statement for Streamlit in app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
 import streamlit as at
-
 st.title("Una página creada por Andres Ruiz, para decirte cuanto te quiere 💙")
 st.write("¡Hola Rashy! ¿Cómo estás? ¿Sabes que eres muy importante para mí y que te quiero mucho?")
